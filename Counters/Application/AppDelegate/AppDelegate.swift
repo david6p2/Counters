59bc6263f8b26b6aebf3f4010c3e307a600f4a33
David Andrés Céspedes <ingdavidcespedes@gmail.com>
2021-04-15 23:11:22 -0500

    Add Main Cooordinator

--- Counters/Application/AppDelegate/AppDelegate.swift
+++ Counters/Application/AppDelegate/AppDelegate.swift
@@ -10,12 +10,17 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    private var coordinator: MainCoordinator?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+        let navController = UINavigationController()
+        navController.navigationBar.prefersLargeTitles = true
+        coordinator = MainCoordinator(navigationController: navController)
+        coordinator?.start()
+
         let window = UIWindow()
-        let presenter = WelcomeViewPresenter()
-        window.rootViewController = WelcomeViewController(presenter: presenter)
+        window.rootViewController = navController
         self.window = window
         window.makeKeyAndVisible()
         return true
