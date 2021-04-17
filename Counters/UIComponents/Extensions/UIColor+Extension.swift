068bb0c86a3ae1978e6d6bfa00b4073124123b80
David Andrés Céspedes <ingdavidcespedes@gmail.com>
2021-04-16 22:50:45 -0500

    Add Colors

---
+++ Counters/UIComponents/Extensions/UIColor+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  UIColor+Extension.swift
+//  Counters
+//
+//  Created by David A Cespedes R on 4/16/21.
+//
+
+import UIKit
+
+extension UIColor {
+    static let accentColorDisable = UIColor.accentColor.withAlphaComponent(0.5)
+    static let accentColor = UIColor(named: "AccentColor")!
+    static let background = UIColor(named: "Background")!
+    static let buttonText = UIColor(named: "ButtonText")!
+    static let descriptionText = UIColor(named: "DescriptionText")!
+    static let disableText = UIColor(named: "DisabledText")!
+    static let countersGreen = UIColor(named: "Green")!
+    static let primaryText = UIColor(named: "PrimaryText")!
+    static let countersRed = UIColor(named: "Red")!
+    static let secondaryText = UIColor(named: "SecondaryText")!
+    static let subtitleText = UIColor(named: "SubtitleText")!
+    static let countersYellow = UIColor(named: "Yellow")!
+}
