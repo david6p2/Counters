729550e2ca919c8653846590cd6afe253751626b
David Andrés Céspedes <ingdavidcespedes@gmail.com>
2021-04-29 00:31:44 -0500

    Add Edit Counters Logic

--- Counters/Application/CountersBoard/CountersBoardViewDelegate.swift
+++ Counters/Application/CountersBoard/CountersBoardViewDelegate.swift
@@ -9,9 +9,16 @@ import UIKit
 
 /// CountersDashboard actions
 protocol CountersBoardViewDelegate: class {
-    func setupNavigationControllerWith(title: String, editBarButton: UIBarButtonItem, searchPlaceholder: String, toolbarItems: [UIBarButtonItem])
+    func setupNavigationControllerWith(title: String,
+                                       editBarButton: UIBarButtonItem,
+                                       selectAllBarButton: UIBarButtonItem?,
+                                       searchPlaceholder: String,
+                                       toolbarItems: [UIBarButtonItem])
     func cellStepperDidChangeValue(_ counterID: String, stepperChangeType: CounterCardView.StepperChangeType)
     func editButtonWasPressed()
+    func selectAllButtonWasPressed()
     func addButtonWasPressed()
+    func trashButtonWasPressed()
+    func shareButtonWasPressed()
     func pullToRefreshCalled()
 }
